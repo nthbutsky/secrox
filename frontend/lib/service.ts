d2dd00fdb87df3485dc2e2db0150c6075128bca5
Denis Butsky <30866948+nthbutsky@users.noreply.github.com>
2024-04-22 15:02:06 +0300

    fix: prepare template

--- frontend/lib/service.ts
+++ frontend/lib/service.ts
@@ -50,20 +50,3 @@ export async function getPostBySlug(slug: string) {
 
   return data?.post;
 }
-
-export async function getPreviewPost(id: string | string[] | undefined, idType = "DATABASE_ID") {
-  const data = await fetchAPI(
-    `
-    query PreviewPost($id: ID!, $idType: PostIdType!) {
-      post(id: $id, idType: $idType) {
-        databaseId
-        slug
-        status
-      }
-    }`,
-    {
-      variables: { id, idType },
-    },
-  );
-  return data.post;
-}

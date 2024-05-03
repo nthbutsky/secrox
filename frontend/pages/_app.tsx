eef07d150044024866e79baf4645c36ba6eb115c
Denis Butsky <30866948+nthbutsky@users.noreply.github.com>
2024-05-04 01:40:29 +0300

    feat: add default font

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
 
 import { AppLayout } from "@/components/Layout";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppLayout>
-      <Component {...pageProps} />
+      <main
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <Component {...pageProps} />
+      </main>
     </AppLayout>
   );
 }
